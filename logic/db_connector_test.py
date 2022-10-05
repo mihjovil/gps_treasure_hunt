af9f57686424f48591c98d6f57a496ae50af02b2
mihjovil <ma.caldas331@gmail.com>
2022-10-05 06:52:06 +0200

    :construction: adding framework for more tests

--- logic/db_connector_test.py
+++ logic/db_connector_test.py
@@ -6,4 +6,12 @@ class TestDbConnector(TestCase):
     def test_create_db(self):
         # TODO
         return
+
+    def test_create_new_config(self):
+        # TODO
+        return
+
+    def test_get_config_from_user(self):
+        # TODO
+        return
     # endregion
