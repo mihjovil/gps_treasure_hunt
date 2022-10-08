f1aac81472b1c0fe4749f2abf25fd2ed2f81e648
mihjovil <ma.caldas331@gmail.com>
2022-10-08 08:27:12 +0200

    :fire: removing outdated comments

--- app.py
+++ app.py
@@ -12,18 +12,3 @@ def create_app():
     if __name__ == '__main__':
         app.run()
     return app
-
-# region help for deployment
-"""
-requirements.txt is needed and pip freeze will create that file with all the packages in the environment
-that might be an overkill, given that the project could use less packages than that.
-Use the following command on your terminal
-- pip install pipreqs
-
-use the following command to create requiremetns.txt file
-- pipreqs path/to/project
-
-that should create the file in the selected path with only the required packages
-
-"""
-# endregion
